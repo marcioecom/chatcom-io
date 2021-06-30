8d387643a2f683fe9c53fa4bfb084357a7dfb378
Márcio Júnior <marcio.mj70@gmail.com>
2021-06-30 14:36:40 -0300

    Create Authenticate feature

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express"
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateUserController } from "./controllers/CreateUserController";
 
 const router = Router()
 
 const createUserController = new CreateUserController()
+const authenticateUserController = new AuthenticateUserController()
 
 router.get('/', function (req, res) {
   return res.sendFile(__dirname + '/views/index.html');
 });
 
 router.post('/users', createUserController.handle)
+router.post('/login', authenticateUserController.handle)
 
 export { router }
---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,41 @@
+import { compare } from "bcryptjs"
+import { sign } from "jsonwebtoken"
+import { getCustomRepository } from "typeorm"
+import { UsersRepository } from "../repositories/UsersRepository"
+
+
+interface IAuthenticateRequest {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+  async execute({ email, password }: IAuthenticateRequest) {
+    const usersRepository = getCustomRepository(UsersRepository)
+
+    const user = await usersRepository.findOne({
+      email
+    })
+
+    if (!user) {
+      throw new Error("Email/Password incorrect")
+    }
+
+    const passwordMatch = await compare(password, user.password)
+
+    if (!passwordMatch) {
+      throw new Error("Email/Password incorrect")
+    }
+
+    const token = sign({
+      email: user.email
+    }, process.env.SECRET, {
+      subject: user.id,
+      expiresIn: "15d"
+    })
+
+    return token
+  }
+}
+
+export { AuthenticateUserService }
